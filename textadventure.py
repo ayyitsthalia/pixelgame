7aabc0dc9c3253a22fc4d84770c221b5c7197646
Thalia Nguyen <thaliaa.nguyen@gmail.com>
2017-08-01 10:45:32 -0700

    Update textadventure.py

--- textadventure.py
+++ textadventure.py
@@ -1,68 +1,113 @@
-#MC = main character
-#every time MC makes the wrong choice she goes back to the beginning in the room with the cat
+start = '''
+One day the main character is at her job, working as a barista,
+and she collapses from overworking. She wakes up in this weird
+dark room and all she sees is a mystical blue cat standing next
+to her. The cat explains that she is currently in this “world” and
+in order to escape, she has to help others in this world and find
+her purpose of life. The cat asks MC if she is ready to embark on
+this journey and MC must choose Yes or No. (types MC’s name in
+this part)
+'''
 
-INTRO: Story background: her name, her relationship with her parents, her situations in college, student loans, part time jobs → 
-she is stressed out and possibly has mental illness (depression)
-
-1st decision (yes/no)
-One day the main character is at her job, working as a barista, and she collapses from overworking. She wakes up  in this weird dark 
-room and all she sees is a mystical blue cat standing next to her. The cat explains that she is currently in this “world” and in order
-to escape, she has to help others in this world and find her purpose of life. The cat asks MC if she is ready to embark on this journey
-and MC must choose Yes or No. (types MC’s name in this part)
-NO (incorrect) : Result: MC wanders around trying to find her way out of the room but fails, ending up in the same room she started in.
-The cat laughs and asks her the question of whether she is ready to embark on the journey continuously until she says yes.
-YES (correct) : Result: The cat guides her out of the room and MC sees this dark, sad, mysterious world with many fantasy creatures.
-All of sudden, a butterfly flies next to her and takes her good-luck charm, her bracelet. The butterfly turns into a fairy and tells 
-the MC that in order to get charm back, she has to collect “pieces”  that are spread throughout this world. The fairy disappears. MC 
-turns over the cat and the cat disappears as well saying, “It’s my nap time. I’ll be back!!!” They both leave MC alone in this long, 
-winding path.
-
-2nd decision: Farmer with the plants and Slime (2 bad choices, 1 correct)
-MC sees an old farmer frowning about something, Curious, MC goes over to the farmer and asks him what happened. The farmer explains
-that his plants are currently sick because of this slime disease (the slimes are the virus) and the harvest is about to come soon. 
+left_hallway = '''
+MC sees an old farmer frowning about something, Curious, MC goes
+over to the farmer and asks him what happened. The farmer explains
+that his plants are currently sick because of this slime disease
+(the slimes are the virus) and the harvest is about to come soon.
 MC is faced with 3 choices:
-#1st CHOICE (BAD) : She makes a bunch of excuses and walks away. Result: A bright light flashes over her eyes, and when she opens her
-eyes again, she is in the dark room and the cat reappears (restart the game)
-#2nd CHOICE (BAD): She gets rid of the slime by using fire. Result: The Slime is gone, which was her goal, but the farmer’s plants ar
-e also gone because she burns his whole farm. The farmer gets mad and kills her so she ended up in the dark room again.
-#3rd CHOICE (GOOD) : She is panicking because she doesn’t know what to do and starts singing randomly to calm the farmer. Result: The
-slimes starts to melt and disappear. The farmer calms down and is happy. MC finds a piece of paper underneath the plants which says
-“Having a soft heart in a cruel world is courage, not weakness.” She scoffs and ignores the quote because she doesn’t understand it.
-The farmer smiles gently and gives her 2 things: a fruit like object that can change color and a little sea shell (long type). MC 
-continues her journey
+'''
+right_hallway = '''
+MC finally sees a pond in the distance and goes over there to rest
+for a while. She hears a cry and looks over to find a horse with
+very moody, dark and dirty colored hair. The horse says that he
+hates himself because he isn’t as pretty as other horses and
+everyone teases him. MC is faced with three choices to help the
+horse.
+'''
 
-3rd decision: Unicorn who is unhappy with himself (2 bad choices, 1 correct)
-MC finally sees a pond in the distance and goes over there to rest for a while. She hears a cry and looks over to find a horse with 
-very moody, dark and dirty colored hair. The horse says that he hates himself because he isn’t as pretty as other horses and everyone 
-teases him. MC is faced with three choices to help the horse. 
-#1st CHOICE (BAD): MC compliments the horse by saying there is nothing wrong with not making friends, you can always be by yourself.
-Society is never nice to you. Result: The horse gets angry and says, “the same goes for you.” MC is back in the dark room again 
-(restart the game)
-#2nd CHOICE (BAD): MC gives the fruit she received from the farmer to the horse, claiming that it will help him look prettier. Result: 
-The horse is allergic to it and has a reaction. As a result he gets mad, and beats her up. She ends up at the dark room again.
-#3rd CHOICE (GOOD): MC gives the seashell she received from the farmer to the horse. Result:  Somehow she dropped it into the pond and
-the shell grows into a beautiful long horn. She puts it on top of the horse’s head and the horse turns into a beautiful unicorn.
-The unicorn appreciates MC and gives her 3 things: a hammer, a flute, and a book. She also finds another piece of paper nearby the
-water saying “let love for yourself set you free of them.” MC sighs and puts the paper back into her pocket along with the first 
-paper and her journey continues.
+common_point = '''
+Night has began to come, and MC decides to take another break
+where there is nothing except for the rocks . Before MC settles
+down on a huge rock, a tiny voice scream “STOPPPP!!” Surprisingly,
+MC stood up and saw a tiny rock looking back at her, pouting.
+The little rock says “You big people keeps on bullying me. I may
+be small but I can beat you up as well.” Behind MC, the big rocks
+start laughing loud and mock this poor little rock. The little
+rock seems to be scared, her eyes are teary but she is still
+trying to fight back. MC shouts “you big immature kids stop it!!”. The big rocks are now laughing at both of them. MC now has 3 choices:
+'''
+common_point = '''
+MC walks for a long time as everything gets darker and darker,
+MC gets a little chill. “Do you miss me?” the cat popped out of
+nowhere and smiles at MC. I’m surprised you are finally here, can you believe it??” MC is still confuse, the cat continues “Now, I think you have enough clues to summon the little fairy back” MC quickly takes out all of the pieces of quotes and put them together. But somehow nothing happened. “What are you doing, MC??” It’s her mom’s voice!! But still, with a not so good tone, she spats at MC “why do leave me all alone in that house? Are you dumping your mom just like your useless dad as well. You r nothing to me. You either go to me or get out of my house
+and live with your terrible dad” (and more insults :D ) MC has 2
+choices:
+'''
 
-4th decision: Bullied little rock (2 bad choices, 1 correct)
-Night has began to come, and MC decides to take another break where there is nothing except for the rocks . Before MC settles down on
-a huge rock, a tiny voice scream “STOPPPP!!” Surprisingly, MC stood up and saw a tiny rock looking back at her, pouting. The little 
-rock says “You big people keeps on bullying me. I may be small but I can beat you up as well.” Behind MC, the big rocks start laughing
-loud and mock this poor little rock. The little rock seems to be scared, her eyes are teary but she is still trying to fight back.
-MC shouts “you big immature kids stop it!!”. The big rocks are now laughing at both of them. MC now has 3 choices:
-#1st CHOICE (BAD) : MC uses the hammer to break the big rocks into smaller rocks. Result:  All the rocks got mad and start attacking
-her as little pieces, locking her deep inside their hard bodies. MC wakes up being at the dark room again.
-#2nd CHOICE (BAD): MC uses the flute to play music and the birds flew over,  MC can smell the fresh grass and flowers bloom. Result:
-but because the flute plays a super squeaky sound which even irritates the rocks even more, so the rocks all get mad at MC and attacks
-MC by crushing her really tight. MC wakes up in the same room again.
-#3rd CHOICE (GOOD): MC quickly opens the book and realize that the book is blank with no information on it. The rocks are getting more aggressive, MC has no choice but to make up something. She thinks of a beautiful garden that her mom used to plant all her favorite flowers in there. Where all her good memories stays in here for the longest time.  The rocks enjoys the birds chirping and they were all singing, blending in with the nature sound. The rocks thanks MC for bringing them joy now that they have something to relax on everyday instead of picking on the small rock. MC saw a little piece of paper in a cave in between the big rocks, she tries to reach for it but the little rock climbed inside and helped her take it out. This time, behind the paper it says “ there is no love without forgiveness and there is no forgiveness without love“. “Hey, since you helped us enjoy happiness after these years, we believe this is the best route you should go” the big rocks say as they seperate themselves, leaving MC a dark yet tempting way to enter. After a long breath, MC enters the entrance.
+common_point = '''
+As her mom vanishes, all the pieces formed into an old drawing
+that MC did back when she was still very small, when her parents
+were still together, when her family is still a happy family
+that is only in her little fairy tale. And the fairy appears,
+“I’ve seen that you love and loved by your family. Because of
+that, I shall grant you a wish, MC. Do you wish to exit this
+world with that old drawing of yours where your reality stays
+there. Or will you rather take back your lucky charm and go
+to a brand new world where your parents are still together ?
+You can only choose once, dear” 
+'''
 
-MC walks for a long time as everything gets darker and darker, MC gets a little chill. “Do you miss me?” the cat popped out of nowhere and smiles at MC. I’m surprised you are finally here, can you believe it??” MC is still confuse, the cat continues “Now, I think you have enough clues to summon the little fairy back” MC quickly takes out all of the pieces of quotes and put them together. But somehow nothing happened. “What are you doing, MC??” It’s her mom’s voice!! But still, with a not so good tone, she spats at MC “why do leave me all alone in that house? Are you dumping your mom just like your useless dad as well. You r nothing to me. You either go to me or get out of my house and live with your terrible dad” (and more insults :D ) MC has 2 choices:
-1st CHOICE (BAD) : “I love both of you” Result: MC shouted out, leaving her mom surprised, “I love you mom, please don’t think of me that way. We are a family. And you know dad loves you more than I do, so you should not be saying such mean things to him.” Mom went even more mad and shouts “So you are with your dad’s side, after how many years I took care of you and this is how you treated me. You disappointed me, MC. I should have just dumped you to those nuns in the church nearby.” Mom runs over to MC and grabs her neck, trying to kill her. MC wakes up again with the dark room. 
-2nd CHOICE (GOOD): “I forgive both.” Result: MC stands there crying but smiling at the same time to the mom, saying “Mom, I know dad has done many bad things to you, but remember this mom, he still loves you a lot. As much as I do. I know both of you has make mistakes and I have learned to forgive you. You don’t have to worry so much about me anymore. I’ll take care of myself.” Mom smiles at MC and slowly disappear, mouthing “We are proud of you, MC” 
 
-As her mom vanishes, all the pieces formed into an old drawing that MC did back when she was still very small, when her parents were still together, when her family is still a happy family that is only in her little fairy tale. And the fairy appears, “I’ve seen that you love and loved by your family. Because of that, I shall grant you a wish, MC. Do you wish to exit this world with that old drawing of yours where your reality stays there. Or will you rather take back your lucky charm and go to a brand new world where your parents are still together ? You can only choose once, dear” 
-CHOOSE THE DRAWING (GOOD):  MC opens her eyes again as she realizes she is in a hospital bed, both of her parents are right next to her. They both cried and hugged her at the same time with joy. “We are very sorry to make you this sick. We could have done better as parents” but MC smiles happily and say “I’m just glad we can be back together as a family” Her long journey has finally end with a deep family bond. 
-CHOOSE THE CHARM (BAD): MC opens her eyes as she realizes she is back in the dark room, seeing a long line “this just proves that you have always wanted a happy life with a family, not with your family. So you deserve no love at all”. And on the news, comes an article saying another young adult has died from overworking herself. (I think I made this too depressing)
+print(start)
+done = False
+while not done:
+    user_input = input("Type 'yes' to go left or 'right' to go right: ")
+    if user_input == "left":
+        print(left_hallway)
+        print(common_point)
+        done = True
+    elif user_input == "right":
+        print(right_hallway)
+        print(common_point)
+        done = True
+    else:
+        print("Please type 'left' or 'right'");
+
+
+
+
+red_doorway = '''
+You walk into the darkness through the red doorway and immediately get the sensation
+that you are falling. You try to scream but can't even hear your own voice. Then
+you start to see your things fly past you. Your phone, you favorite pair of shoes,
+your favorite toy, pictures from your childhood. You see memories, but you can't
+feel them. They are all happening to this other person that looks like you but is
+a stranger. You hear your mother's voice, "Why?". Then you hit something.
+'''
+
+white_doorway = '''
+You walk into the darkness through the white doorway and feel yourself pulled up
+like you're in a vacuum, but gently. A soft breeze that smells of vanilla seems
+to envelope you and you sense two large hands underneath pushing you up. You weren't
+being sucked up but carried up. You hear your mothers voice again, "Why?". Then
+you stop.
+'''
+end_of_story = '''
+You're in bed. You stand up in bed and turn off your alarm. It's time to
+to start your day. You put on your pair of mismatched slippers, red and white, and
+go to the bathroom.
+'''
+
+done = False
+while not done:
+    user_input = input("Type 'red' to go into the red doorway or 'white' to into the white doorway: ")
+    if user_input == "red":
+        print(red_doorway)
+        print(end_of_story)
+        done = True
+    elif user_input == "white":
+        print(white_doorway)
+        print(end_of_story)
+        done = True
+    else:
+        print("Please type 'red' or 'white'");
