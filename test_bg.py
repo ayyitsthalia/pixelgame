fb201a0a245412a234f741b7592cf8dbd37425f0
lazynhi <conniewongntn@gmail.com>
2017-08-03 09:24:12 -0700

    Update test_bg.py
    you can change the background !!! :D

--- test_bg.py
+++ test_bg.py
@@ -24,86 +24,63 @@ class Cat(object):
         elif key[pygame.K_LEFT]:
             self.x -= dist
 
-    def update(self):
-        if self.x >= 640:
-            self.x = 0
-        elif self.x <= 0:
-            self.x = 640
-        if self.y >= 400:
-            self.y = 0
-        elif self.y <= 0:
-            self.y = 400
-
     def draw(self, surface):
         """ Draw on surface"""
         surface.blit(self.image, (self.x, self.y))
 
-class switch_screen(object):
-    def __new__(cls, n):
-        if self.x >= 640:
-            touch_edge = True
-            new_cls = dark_room
-        else:
-            new_cls = forest
-        instance = super(switch_screen, new_cls).__new__(new_cls, n)
 
-        if new_cls != cls:
-            instance.__init__(n)
-        return instance
-
-    def update(self):
-        if self.x >= 640:
-            self.x = 0
-        elif self.x <= 0:
-            self.x = 640
-        if self.y >= 400:
-            self.y = 0
-        elif self.y <= 0:
-            self.y = 400
-
-class dark_room(object):
-    def __init__(self, n):
-        self.n = n
-        bg1 = pygame.image.load("backgroundtest.png").convert_alpha()
-        screen.fill(0,0,0)
-        screen.blit(bg1, (0,0))
-
-class forest(object):
-    def __init__(self, n):
-        self.n = n
-        bg2 = pygame.image.load("background2.png").convert_alpha()
-        screen.fill(0,0,0)
-        screen.blit(bg2, (0,0))
-
-
+## Start of main program
 pygame.init()
 screen = pygame.display.set_mode((640, 400))
 
-# background images
-#bg1 = pygame.image.load("backgroundtest.png").convert_alpha()
-#bg2 = pygame.image.load("background2.png").convert_alpha()
+##background
+screen.fill((0,0,0)) ## to clean background
 
+bg1 = pygame.image.load("bg1.jpg")
 
+bg2 = pygame.image.load("bg2.jpg")
+
+currentScreen =  bg1
 cat = Cat()
 clock = pygame.time.Clock()
+#StartScreen = Background_img()
 
 running = True
-touch_edge = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             running = False
 
+    if cat.y >= 400:
+        if currentScreen == bg1:
+            cat.y = 0
+            screen.fill((0,0,0))
+            screen.blit(bg2,(0,0))
+            currentScreen = bg2
 
+
+        elif currentScreen == bg2:
+            screen.fill((0,0,0))
+            screen.blit(bg2,(0,0))
+            currentScreen = bg2
+            cat.y = 400
+    elif cat.y <= 0:
+        if currentScreen == bg2:
+            cat.y = 400
+            screen.fill((0,0,0))
+            screen.blit(bg1,(0,0))
+            currentScreen = bg1
+
+        elif currentScreen == bg1:
+            screen.fill((0,0,0))
+            screen.blit(bg1,(0,0))
+            currentScreen = bg1
+            cat.y = 0
+ ###### The sprite is going from top to bottom :D
 
 
     cat.handle_keys()
-    cat.update()
     cat.draw(screen)
-    switch_screen.update()
-
     pygame.display.update()
-
-
     clock.tick(40)
