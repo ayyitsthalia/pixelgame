c58af844ab1157061b7b3a62b65fbf29da4c3383
samanthatam2001 <samanthatam123@yahoo.com>
2017-08-03 10:06:12 -0700

    Add files via upload

--- pixelgametest.py
+++ pixelgametest.py
@@ -1,9 +1,10 @@
 import pygame
 import os
+import time
 
 #images/sprite
-img_path = 'sprite_horse2.png'
-img_bg = 'background640x400.png'
+img_path = 'sprite_charizard2.png'
+img_bg = 'background1.png'
 img_MC = 'sprite_MC02.png'
 
 class Cat(object):
@@ -14,9 +15,9 @@ class Cat(object):
         self.y = 0
 
     def handle_keys(self):
-        """ Handle Keys """
+        """Handle Keys"""
         key = pygame.key.get_pressed()
-        dist = 5
+        dist = 10
         if key[pygame.K_DOWN]:
             self.y += dist
         elif key[pygame.K_UP]:
@@ -27,28 +28,28 @@ class Cat(object):
             self.x -= dist
 
     def update(self):
-        if self.x >= 640:
+        if self.x >= 800:
             self.x = 0
         elif self.x <= 0:
-            self.x = 640
-        if self.y >= 400:
+            self.x = 800
+        if self.y >= 600:
             self.y = 0
         elif self.y <= 0:
-            self.y = 400
+            self.y = 600
 
     def draw(self, surface):
-        """ Draw on surface"""
+        """Draw on surface"""
         surface.blit(self.image, (self.x, self.y))
 
 class MC(object):
     def __init__(self):
         """The constructor of the class"""
         self.image = pygame.image.load(img_MC)
-        self.x = 0
-        self.y = 0
+        self.x = 50
+        self.y = 50
 
-    def Keys(self):
-        """ WASD Keys """
+    def moving_keys(self):
+        """Handle Keys"""
         key = pygame.key.get_pressed()
         dist = 5
         if key[pygame.K_s]:
@@ -60,29 +61,28 @@ class MC(object):
         elif key[pygame.K_a]:
             self.x -= dist
 
-    def update(self):
-        if self.x >= 640:
+    def loop(self):
+        if self.x >= 800:
             self.x = 0
         elif self.x <= 0:
-            self.x = 640
-        if self.y >= 400:
+            self.x = 800
+        if self.y >= 600:
             self.y = 0
         elif self.y <= 0:
-            self.y = 400
+            self.y = 600
 
-    def draw(self, surface):
-        """ Draw on surface"""
+    def paint(self, surface):
+        """Draw on surface"""
         surface.blit(self.image, (self.x, self.y))
 
-
 pygame.init()
-screen = pygame.display.set_mode((640, 400))
+screen = pygame.display.set_mode((800, 600))
 
 screen.fill(((0,0,0)))
 bg1 = pygame.image.load(img_bg).convert_alpha()
 screen.blit(bg1,(0,0))
 
-MC = MC()
+mc = MC()
 cat = Cat()
 clock = pygame.time.Clock()
 
@@ -95,6 +95,9 @@ while running:
     screen.fill(((0,0,0)))
     screen.blit(bg1,(0,0))
 
+    mc.moving_keys()
+    mc.loop()
+    mc.paint(screen)
     cat.handle_keys()
     cat.update()
     cat.draw(screen)
